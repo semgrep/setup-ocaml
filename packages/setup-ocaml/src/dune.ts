4f6d966e7e6fced1df7ad8cf9701d91b9ca2f944
yosefAlsuhaibani <yosefalsuhaibani@gmail.com>
2026-03-22 13:58:41 -0400

    fixup! fixup! fix: do not try to trim dune cache

--- packages/setup-ocaml/src/dune.ts
+++ packages/setup-ocaml/src/dune.ts
@@ -1,6 +1,5 @@
 import * as core from "@actions/core";
 import { exec } from "@actions/exec";
-import * as github from "@actions/github";
 
 export async function installDune() {
   await core.group("Installing dune", async () => {
